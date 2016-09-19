31146ad1cc6f2bb058308492d35a2744a193bdcf
ashleyvernon <avernon526@gmail.com>
2016-09-18 18:48:35 -0700

    setting up routes

--- scripts/app.js
+++ scripts/app.js
@@ -2,66 +2,26 @@ console.log('Sanity check JS is working!');
 
 angular
 	.module('book', ['ngRoute'])
-	.controller('BooksIndexController', BooksIndexController);
+	.config(config);
 
-//tells controller that we'd like to have access to http module
-BooksIndexController.$inject = ['$http'];
-//passes http into controller function
-function BooksIndexController($http) {
-	var vm = this;
-	// vm.newBook = {};
-	$http({
-		method: 'GET',
-		url: 'https://super-crud.herokuapp.com/books'
-	}).then(function successCallback(response) {
-		vm.books = response.data.books;
-	}, function errorCallback(reponse) {
-		console.log('There was an error getting the data', response);
+
+function config ($routeProvider, $locationProvider) {
+	$routeProvider
+		.when('/', {
+			templateUrl: 'templates/books',
+			controllerAs: 'booksIndexCtrl', 
+			controller: 'BooksIndexController'
+		});
+		//route is going to show us details of individual books
+		.when('/:id', {
+			templateUrl: 'templates/books-show',
+			controllerAs: 'booksShowCtrl',
+			controller: 'BooksShowController'
+		});
+
+	$locationProvider.html5Mode({
+		enabled: true,
+		requireBase: false
 	});
-
-	vm.createBook = function() {
-		$http({
-			method: 'POST',
-			url: 'https://super-crud.herokuapp.com/books',
-			data: {
-				title: newTitle,
-				author: newAuthor,
-				image: newImage,
-				releaseDate: newReleaseDate
-			}
-		}).then(function successCallback(response) {
-			req.params.body(response);
-			console.log(req.params.body(response));
-		}, function errorCallback(response) {
-			console.log('There was an error posting the data', response);
-		});
-	}
-
-	vm.updateBook = function(book) {
-		$http({
-			method: 'PUT',
-			url: 'https://super-crud.herokuapp.com/books' + book._id,
-		}).then(function successUpdateCallback(response) {
-			
-		})
-	}
-
-	vm.deleteBook = function(book) {
-		$http({
-			method: 'DELETE',
-			url: 'https://super-crud.herokuapp.com/books' + book._id,
-		}).then(function successDeleteCallback(json) {
-			var index = vm.books.indexOf(book);
-			vm.books.splice(index, 1);
-			console.log('book delete response data:', response.data);
-			}, function errorCallback(response) {
-			console.log('There was an error deleting the data', response);
-		});
-	}	
-	// vm.newBook = {
-	// 	title: '',
-	// 	author: '',
-	// 	image: '',
-	// 	releaseDate: ''
-	// };
 }
+//tells controller that we'd like to have access to http module
